63a5e3b92786cb6dfd47b51641e5fd0fbbae2d09
Christian Fei <info@christian-fei.com>
2013-07-21 13:12:05 +0200

    removed .demeteorized, update

--- opentalk.js
+++ opentalk.js
@@ -1,83 +1,136 @@
 Messages = new Meteor.Collection('Messages');
-
+Rooms = new Meteor.Collection('Rooms');
 
 if (Meteor.isClient) {
+  console.log(Meteor.absoluteUrl({rootUrl:'http://opentalk.me'}));
 
-  var lastInsertId=0,
-  text='',
-  t=0;
-
-  Session.set('chatRoomId', null)
-  console.log(Meteor.user());
-  
-  /*
-  Deps.autorun(function () {
-    Meteor.subscribe("MessagesChatroom", Session.get("chatroom"));
-  });
-  */
+  var lastInsertId=0, //ID of the last inserted message
+  text='', //current message text
+  t=0, //current timestamp
+  mSub, //Message subscription
+  rSub=null; //Room subscription
 
   Template.selectChatRoom.events({
-    'keyup #chatRoom': function(evnt,tmplt){
-      Session.set('chatroom',tmplt.find('#chatRoom').value);
-      Meteor.subscribe('MessagesChatroom',Session.get('chatroom'));
+    'keyup #roomID': function(evnt,tmplt){
+      var room = tmplt.find('#roomID').value;
+      if(room.length){
+        Session.set('roomID',tmplt.find('#roomID').value);
+        mSub=Meteor.subscribe('MessagesChatroom',Session.get('roomID'));
+      }else{
+        Session.set('roomID',null);
+        if(mSub)
+          mSub.stop();
+      }
     }
   });
 
-  Template.chatWindow.userName = function(){
+  Template.messagesList.userName = function(){
     return Meteor.user().profile.name;
-  }
-  Template.chatWindow.messages = function(){
-    return Messages.find(
-      {'chatRoomId':Session.get('chatroom')}
-      ,{sort: {timestamp: 1}}
-    );
   };
-  Template.chatWindow.enabled = function(){
-    if(Meteor.user() && Session.get('chatroom'))
+
+  function iAmWriting(){
+    console.log('liid  ' + Session.get('lastInsertId'));
+    if(Session.get('lastInsertId')){
+      var m,p=0,
+      ml = Messages.find().fetch().length,
+      lm = Messages.find().fetch()[ml-1],
+      lmid;
+      if(lm)
+        lmid = lm._id;
+      //console.log(lmid);
+      if(m=Messages.find({_id:lmid,userID:Meteor.user()._id}).fetch()[0])
+        if(m.userID === Meteor.user()._id){
+          return true;
+        }
+      return false;
+    }
+    return false;    
+  }
+
+  Template.messagesList.messages = function(){
+    /*
+    TODO
+      - do not return the last message to the user that is typing it
+    */
+    var ml = Messages.find({'roomID':Session.get('roomID')}).fetch().length;
+
+    console.log(iAmWriting());
+    if(iAmWriting()){
+      console.log(ml);
+      if(ml <= 1)
+        return [];
+      return Messages.find(
+        {'roomID':Session.get('roomID')}
+        ,{sort: {timestamp: 1},limit:ml -1});
+    }else
+      return Messages.find(
+        {'roomID':Session.get('roomID')}
+        ,{sort: {timestamp: 1}}
+      );  
+  };
+  Template.messagesList.loggedIn=Template.selectChatRoom.loggedIn=function(){
+    if(Meteor.user())
       return true;
     return false;
-  }
-  Template.chatWindow.events({
+  };
+  Template.messagesList.roomSelected = function(){
+    if(Session.get('roomID'))
+      return true;
+    return false;
+  };
+  Template.messagesList.events({
     'keyup #mymessage' : function(evnt,tmplt){
       if(Meteor.user()){
 
         text = tmplt.find('#mymessage').value;
         t = Date.now();
 
-        /*First message (first keystroke) being sent*/
-        if(!lastInsertId)
-          lastInsertId = Messages.insert(
-            {userId:Meteor.user()._id
-            ,username:Meteor.user().profile.name
-            ,chatRoomId:Session.get('chatroom')
-            ,text:text
-            ,timestamp:t});
+        /*First message/first keystroke being sent*/
+        if(!Session.get('lastInsertId') && text.length){
+          Session.set(
+            'lastInsertId',
+            Messages.insert(
+              {userID:Meteor.user()._id
+              ,username:Meteor.user().profile.name
+              ,roomID:Session.get('roomID')
+              ,text:text
+              ,timestamp:t}
+            )
+          );
+          return;
+        }
 
         if(evnt.keyCode === 13){
           if(text.length){
             //new Message
-            lastInsertId = Messages.insert(
-              {userId:Meteor.user()._id
-              ,username:Meteor.user().profile.name
-              ,chatRoomId:Session.get('chatroom')
-              ,text:'',
-              timestamp:t}
+            Session.set(
+              'lastInsertId',
+              Messages.insert(
+                {userId:Meteor.user()._id
+                ,username:Meteor.user().profile.name
+                ,roomID:Session.get('roomID')
+                ,text:'',
+                timestamp:t}
+              )
             );
             tmplt.find('#mymessage').value = '';
+          } else {
+            Messages.remove({_id:''+Session.get('lastInsertId')});
+            Session.set('lastInsertId',null);
           }
-        }else{
+        } else {
           if(text.length){
             Messages.update(
-              {_id:''+lastInsertId}
+              {_id:''+Session.get('lastInsertId')}
               ,{$set : {
                   text:text
                   ,timestamp:t
                 }
               }
             );
-          }else{
-            Messages.remove({_id:''+lastInsertId});
-            lastInsertId=null;
+          } else {
+            Messages.remove({_id:''+Session.get('lastInsertId')});
+            Session.set('lastInsertId',null);
           }
         }
       }
@@ -87,15 +140,17 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+
+
   Messages.allow({
     insert : function(){return true}
     ,update : function(){return true}
     ,remove : function(){return true}
   });
 
-  Meteor.publish('MessagesChatroom',function(chatRoomId){
+  Meteor.publish('MessagesChatroom',function(roomID){
     return Messages.find(
-      {chatRoomId:chatRoomId}
+      {roomID:roomID}
     )
    }
   );
