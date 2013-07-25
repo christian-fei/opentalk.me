dc4e13aa9d4815f7c43cbdd0346518dce2d47bc7
Christian Fei <info@christian-fei.com>
2013-07-25 11:15:39 +0200

    working

---
+++ collections/collections.js
@@ -0,0 +1,2 @@
+Messages = new Meteor.Collection('Messages');
+OnlineUsers = new Meteor.Collection('OnlineUsers');
---
+++ server/main.js
@@ -0,0 +1,43 @@
+Messages.allow({
+  insert : function(){return true}
+  ,update : function(){return true}
+  ,remove : function(){return true}
+});
+OnlineUsers.allow({
+  insert : function(){return true}
+  ,update : function(){return true}
+  ,remove : function(){return true}
+});  
+
+Meteor.publish('MessagesChatroom',function(roomid){
+  return Messages.find(
+    {
+      roomid:roomid
+    }
+  );
+});
+
+Meteor.publish('usersOnlineInThisRoom',function(roomid){
+  return OnlineUsers.find(
+    {
+      roomid:roomid
+    }
+  );
+});
+
+
+Meteor.methods({
+  removeMessagesOfUserInRoom : function(userid,roomid){
+    Messages.remove({userid:userid,roomid:roomid}, function(){console.log('messages of user ' + userid + ' removed from room ' + roomid);});
+  },
+  clog : function(s){
+    console.log(s);
+  },
+  removeOnlineUserFromRoom : function(userid,roomid){
+    OnlineUsers.remove({userid:userid,roomid:roomid});
+  }
+});
+
+Meteor.startup(function () {
+  // code to run on server at startup
+});
