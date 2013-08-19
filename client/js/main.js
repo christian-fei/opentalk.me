85f5843c8798bb39cb9a5a910e48cfa65b4490a3
Christian Fei <info@christian-fei.com>
2013-08-20 01:46:07 +0200

    social,icons not w

--- client/js/main.js
+++ client/js/main.js
@@ -540,6 +540,21 @@ Template.loginForm.events({
 	      //notify
 	    }
 	  }
+	},
+	'click .serviceLogin': function(e,t){
+		e.preventDefault();
+		if(e.srcElement.classList.contains('twitter') ) {
+			Meteor.loginWithTwitter();
+		}else
+		if(e.srcElement.classList.contains('facebook') ) {
+			Meteor.loginWithFacebook();
+		}else
+		if(e.srcElement.classList.contains('github') ) {
+			Meteor.loginWithGithub();
+		}else
+		if(e.srcElement.classList.contains('google') ) {
+			Meteor.loginWithGoogle();
+		}
 	}
 });
 
