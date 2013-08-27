60fc121e15f8fb7c073d760d3a2ff64674545877
Christian Fei <info@christian-fei.com>
2013-08-27 23:44:18 +0200

    css

--- client/js/markdown.js
+++ client/js/markdown.js
@@ -9,11 +9,11 @@ markdown = {
         // r = r.replace(/>(.*)/gm, '<blockquote>$1</blockquote>');
 
         // h1 - h6 and hr
-        r = r.replace(/(^|\s)######(.*)/gm, '<h6>$1</h6>');
-        r = r.replace(/(^|\s)#####(.*)/gm, '<h5>$1</h5>');
-        r = r.replace(/(^|\s)####(.*)/gm, '<h4>$1</h4>');
-        r = r.replace(/(^|\s)###(.*)/gm, '<h3>$1</h3>');
-        r = r.replace(/(^|\s)##(.*)/gm, '<h2>$1</h2>');
+        r = r.replace(/(^|\s)######(.*)/gm, '<h6>$2</h6>');
+        r = r.replace(/(^|\s)#####(.*)/gm, '<h5>$2</h5>');
+        r = r.replace(/(^|\s)####(.*)/gm, '<h4>$2</h4>');
+        r = r.replace(/(^|\s)###(.*)/gm, '<h3>$2</h3>');
+        r = r.replace(/(^|\s)##(.*)/gm, '<h2>$2</h2>');
         r = r.replace(/(^|\s)#(.*)/gm, '<h1>$2</h1>');
         r = r.replace(/(^|\s)[-*][-*][-*]+/gm, '<hr>');
         
@@ -22,16 +22,16 @@ markdown = {
         r = r.replace(/__(.*?)__/g, '<strong>$1</strong>');
         //italics
         r = r.replace(/\*(.*?)\*/g, '<em>$1</em>');
-        r = r.replace(/(^|\s)_(.*?)_/g, '<em>$1</em>');
+        r = r.replace(/(^|\s)_(.*?)_/g, '<em>$2</em>');
         //strike through
         r = r.replace(/~~(.*?)~~/g, '<strike>$1</strike>');
         // r = r.replace(new RegExp('//(((?!https?://).)*?)//', 'g'), '<em>$1</em>');
         r = r.replace(/``(.*?)``/gm, '<code>$1</code>');
         
         // unordered lists with 
-        r = r.replace(/(^|\s)\+(.*)/gm, '<ul><li>$1</li></ul>');
-        r = r.replace(/(^|\s)\-(.*)/gm, '<ul><li>$1</li></ul>');
-        r = r.replace(/(^|\s)\*(.*)/gm, '<ul><li>$1</li></ul>');
+        r = r.replace(/(^|\s)\+(.*)/gm, '<ul><li>$2</li></ul>');
+        r = r.replace(/(^|\s)\-(.*)/gm, '<ul><li>$2</li></ul>');
+        r = r.replace(/(^|\s)\*(.*)/gm, '<ul><li>$2</li></ul>');
 
 
         // restore the preformatted and unformatted blocks
