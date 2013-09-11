1b9087c226d7c812ac568d25188a57fff8a113a2
Christian Fei <info@christian-fei.com>
2013-09-11 21:33:53 +0200

    fixed REstart forever

--- fastdeploy.sh
+++ fastdeploy.sh
@@ -12,4 +12,4 @@ cd .demeteorized
 echo '====npm install===='
 npm install
 echo '====starting forever daemon===='
-ROOT_URL=http://opentalk.me:3000 PORT=3000 MONGO_URL=mongodb://localhost:27017/opentalk forever start /root/nodestuff/opentalk.me/.demeteorized/main.js  >> /root/nodestuff/opentalk.me/log.txt 2>&1
+ROOT_URL=http://opentalk.me:3000 PORT=3000 MONGO_URL=mongodb://localhost:27017/opentalk forever restart /root/nodestuff/opentalk.me/.demeteorized/main.js  >> /root/nodestuff/opentalk.me/log.txt 2>&1
