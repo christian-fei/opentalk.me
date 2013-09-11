3ff5d243a8b9b43dfd60a02f14346fa345c6df40
Christian Fei <info@christian-fei.com>
2013-09-11 21:32:39 +0200

    update fastdeploy.sh

--- fastdeploy.sh
+++ fastdeploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+echo '====checking out===='
 git checkout .
+echo '====pulling latest changes===='
 git pull
+echo '====removing .demeteorized===='
 rm -rf .demeteorized
+echo '====demeteorizing===='
 demeteorizer
+echo '====cd .demeteorized===='
 cd .demeteorized
+echo '====npm install===='
 npm install
-sudo ROOT_URL=http://opentalk.me:3000 PORT=3000 MONGO_URL=mongodb://localhost:27017/opentalk forever start /root/nodestuff/opentalk.me/.demeteorized/main.js  >> /root/nodestuff/opentalk.me/log.txt 2>&1
+echo '====starting forever daemon===='
+ROOT_URL=http://opentalk.me:3000 PORT=3000 MONGO_URL=mongodb://localhost:27017/opentalk forever start /root/nodestuff/opentalk.me/.demeteorized/main.js  >> /root/nodestuff/opentalk.me/log.txt 2>&1
