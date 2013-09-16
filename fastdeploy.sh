f27ed35cc951548c3e089215d46869df54d61e51
Christian Fei <info@christian-fei.com>
2013-09-16 21:42:18 +0200

    no :3000

--- fastdeploy.sh
+++ fastdeploy.sh
@@ -15,7 +15,7 @@ if curl --output /dev/null -sI "http://athmosphere.meteor.com/"; then
 	echo '====npm install===='
 	npm install
 	echo '====starting forever daemon===='
-	ROOT_URL=http://opentalk.me:3000 PORT=3000 MONGO_URL=mongodb://localhost:27017/opentalk forever restart /root/nodestuff/opentalk.me/.demeteorized/main.js  >> /root/nodestuff/opentalk.me/log.txt 2>&1
+	ROOT_URL=http://opentalk.me PORT=3000 MONGO_URL=mongodb://localhost:27017/opentalk forever restart /root/nodestuff/opentalk.me/.demeteorized/main.js  >> /root/nodestuff/opentalk.me/log.txt 2>&1
 else
 	echo '====athmosphere is down==== '
 fi
