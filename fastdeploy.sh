c5d389df9edd2579b6a5378c1b1a7199d9c5651c
Christian Fei <info@christian-fei.com>
2013-09-16 21:19:43 +0200

    it probably breaks2 fixes

--- fastdeploy.sh
+++ fastdeploy.sh
@@ -10,4 +10,4 @@ demeteorizer
 echo '====cd .demeteorized===='
 cd .demeteorized
 echo '====starting forever daemon===='
-ROOT_URL=http://opentalk.me PORT=3000 MONGO_URL=mongodb://localhost:27017/opentalk forever restart /root/nodestuff/opentalk.me/.demeteorized/main.js  >> /root/nodestuff/opentalk.me/log.txt 2>&1
+ROOT_URL=http://opentalk.me:3000 PORT=3000 MONGO_URL=mongodb://localhost:27017/opentalk forever restart /root/nodestuff/opentalk.me/.demeteorized/main.js  >> /root/nodestuff/opentalk.me/log.txt 2>&1
