7cb1d6ecd6368c24b140438dbcc2b447859a6226
Christian Fei <info@christian-fei.com>
2013-11-01 23:40:39 +0100

    better modals

--- client/views/welcome.js
+++ client/views/welcome.js
@@ -1,19 +1,21 @@
 var statsUpdater=null;
-function setGlobalStats(){
+function renderGlobalStats(){
 	if(!statsUpdater){
+		getGlobalStats();
 		statsUpdater = setInterval(function(){
-			// 
-			//setInterval to retrieve info periodcally
-			Meteor.call('globalMessagesCount',function(err,res){$('#globalMessagesCount').text(res);});
-			Meteor.call('globalRoomsCount',function(err,res){$('#globalRoomsCount').text(res);});
-			Meteor.call('globalOnlineUsersCount',function(err,res){$('#globalOnlineUsersCount').text(res);});
-			Meteor.call('globalRegisteredUsersCount',function(err,res){$('#globalRegisteredUsersCount').text(res);});
+			getGlobalStats();
 		},1500);
 	}
 }
+function getGlobalStats(){
+	Meteor.call('globalMessagesCount',function(err,res){$('#globalMessagesCount').text(res);});
+	Meteor.call('globalRoomsCount',function(err,res){$('#globalRoomsCount').text(res);});
+	Meteor.call('globalOnlineUsersCount',function(err,res){$('#globalOnlineUsersCount').text(res);});
+	Meteor.call('globalRegisteredUsersCount',function(err,res){$('#globalRegisteredUsersCount').text(res);});
+}
 
 Template.welcome.rendered = function(){
-	setGlobalStats();
+	renderGlobalStats();
 
 	$('.Modal:not(.bound)').addClass('bound').bind('click',function(e){e.stopPropagation()});
 	$('.toggle-Modal:not(.bound)').addClass('bound').bind('click',function(){$(this).toggleClass('is-Hidden')});
