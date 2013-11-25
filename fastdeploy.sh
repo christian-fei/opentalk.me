f9e58a91fafc6677c578c31af1ae918c81f1fdc6
Christian Fei <info@christian-fei.com>
2013-11-25 21:18:56 +0100

    fd

--- fastdeploy.sh
+++ fastdeploy.sh
@@ -13,9 +13,10 @@ if curl --output /dev/null -sI "http://athmosphere.meteor.com/"; then
 	echo '====demeteorizing===='
 	demeteorizer
 	echo '====cd .demeteorized===='
-	cp .package.json .demeteorized/package.json
+#	cp .package.json .demeteorized/package.json
 	cd .demeteorized
 	echo '====npm install===='
+	npm install fibers@1.0.1
 	npm install
 	ROOT_URL=http://opentalk.me:3000 PORT=3000 MONGO_URL=mongodb://localhost:27017/opentalk forever start /root/nodestuff/opentalk.me/.demeteorized/main.js  >> /root/nodestuff/opentalk.me/log.txt 2>&1
 	echo '====starting forever daemon===='
