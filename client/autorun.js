9d3b0ac28d25c14bacc758beba4751d91876e2b7
Christian Fei <info@christian-fei.com>
2013-12-12 20:17:42 +0100

    autorun;

--- client/autorun.js
+++ client/autorun.js
@@ -4,17 +4,22 @@ Deps.autorun(function(){
 		Meteor.subscribe('MessagesAdmin');
 		Meteor.subscribe('UsersAdmin');
 	}
+});
+Deps.autorun(function(){
 
+	Meteor.subscribe('roomTags',Session.get('roomid'));
+
+	getAvatar();
+});
+Deps.autorun(function(){
 	if(Meteor.user()){
 		Meteor.subscribe('userData');
 	}
+});
+Deps.autorun(function(){
 	if(Session.get('avatar')){
 		//when the avatar changes go online 
 		// to update the avatar to all the clients
 		goOnline();
 	}
-
-	Meteor.subscribe('roomTags',Session.get('roomid'));
-
-	getAvatar();
 });
